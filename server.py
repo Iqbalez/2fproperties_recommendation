6905e6c04e9c90be94849cd542118f2ffbb98d1d
derrick <108317502+derricktan23@users.noreply.github.com>
2025-04-16 20:52:04 +0800

    Update server.py

--- server.py
+++ server.py
@@ -242,4 +242,4 @@ def feedback():
         db.session.remove()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=True)
